ebf1290444f015e037646209c690cb8abf62ac0f
HyunjungKim <96401185+hyunjung222@users.noreply.github.com>
2023-06-21 16:51:05 +0900

    Upload xcframework

---
+++ DcentBiometric.xcframework/ios-arm64/DcentBiometric.framework/Headers/DcentBiometric.h
@@ -0,0 +1,19 @@
+//
+//  DcentBiometric.h
+//  DcentBiometric
+//
+//  Created by iotrustMac01 on 24/05/2019.
+//  Copyright © 2019 IoTrust Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DcentBiometric.
+FOUNDATION_EXPORT double dcentBiometricVersionNumber;
+
+//! Project version string for DcentBiometric.
+FOUNDATION_EXPORT const unsigned char dcentBiometricVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DcentBiometric/PublicHeader.h>
+
+
