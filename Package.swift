373f49b852793f8c23fd087b4f34df2a9bfda47f
hyunjung222 <hjkim@iotrust.kr>
2023-06-22 13:48:57 +0900

    remove XCGLogger

--- Package.swift
+++ Package.swift
@@ -11,14 +11,14 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
-        .package(url: "https://github.com/DaveWoodCom/XCGLogger.git", from: "7.0.1"),
+//        .package(url: "https://github.com/DaveWoodCom/XCGLogger.git", from: "7.0.1"),
         .package(url: "https://github.com/attaswift/BigInt.git", from: "5.3.0")
     ],
     targets: [
         //        Target
         .binaryTarget(name: "DcentBiometric", path: "DcentBiometric.xcframework"),
         .target(name: "DcentBiometricFrameworkBundle",
-                dependencies: [ .target(name: "DcentBiometric"), .product(name: "SwiftProtobuf", package: "swift-protobuf"), .product(name: "XCGLogger", package: "XCGLogger"), .product(name: "BigInt", package: "BigInt"), .product(name: "ObjcExceptionBridging", package: "XCGLogger")],
+                dependencies: [ .target(name: "DcentBiometric"), .product(name: "SwiftProtobuf", package: "swift-protobuf"), /*.product(name: "XCGLogger", package: "XCGLogger"),*/ .product(name: "BigInt", package: "BigInt")],
                 path: "DcentBiometricFrameworkBundle")
          
     ]
