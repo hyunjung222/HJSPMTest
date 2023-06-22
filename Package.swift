7dd069333799ff86f681361218888a34d9b63545
hyunjung222 <hjkim@iotrust.kr>
2023-06-22 10:30:58 +0900

    fix error

--- Package.swift
+++ Package.swift
@@ -11,13 +11,15 @@ let package = Package(
     ],
     dependencies: [
         .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
-        .package(name: "XCGLogger", url: "https://github.com/DaveWoodCom/XCGLogger.git", majorVersion: 7),
+        .package(name: "XCGLogger", url: "https://github.com/DaveWoodCom/XCGLogger.git", from: "7.0.1"),
         .package(name: "BigInt", url: "https://github.com/attaswift/BigInt.git", from: "5.3.0")
     ],
     targets: [
-        .binaryTarget(name: "DcentBiometric", path: "DcentBiometric.xcframework")
-         .target(name: "DcentBiometricFrameworkBundle",
-            dependencies: [ .target(name: "DcentBiometricTest"), "SwiftProtobuf", "XCGLogger", "BigInt"],
-            path: "DcentBiometricFrameworkBundle")
+        //        Target
+        .binaryTarget(name: "DcentBiometric", path: "DcentBiometric.xcframework"),
+        .target(name: "DcentBiometricFrameworkBundle",
+                dependencies: [ .target(name: "DcentBiometric"), "SwiftProtobuf", "XCGLogger", "BigInt"],
+                path: "DcentBiometricFrameworkBundle")
+         
     ]
 )
