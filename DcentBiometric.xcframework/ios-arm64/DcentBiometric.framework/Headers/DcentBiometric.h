329e43ce3a5ce2ad42c07b44f8e12627f2a6a48d
hyunjung222 <hjkim@iotrust.kr>
2023-06-22 18:50:26 +0900

    spm xcframework

--- DcentBiometric.xcframework/ios-arm64/DcentBiometric.framework/Headers/DcentBiometric.h
+++ DcentBiometric.xcframework/ios-arm64/DcentBiometric.framework/Headers/DcentBiometric.h
@@ -2,17 +2,16 @@
 //  DcentBiometric.h
 //  DcentBiometric
 //
-//  Created by iotrustMac01 on 24/05/2019.
-//  Copyright © 2019 IoTrust Co., Ltd. All rights reserved.
+//  Created by musicgi on 2023/06/22.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for DcentBiometric.
-FOUNDATION_EXPORT double dcentBiometricVersionNumber;
+FOUNDATION_EXPORT double DcentBiometricVersionNumber;
 
 //! Project version string for DcentBiometric.
-FOUNDATION_EXPORT const unsigned char dcentBiometricVersionString[];
+FOUNDATION_EXPORT const unsigned char DcentBiometricVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <DcentBiometric/PublicHeader.h>
 
