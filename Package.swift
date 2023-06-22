6dadbe577f9acba383b29a82ce4ce55bda4c2078
hyunjung222 <hjkim@iotrust.kr>
2023-06-22 11:25:30 +0900

    modify dependency

--- Package.swift
+++ Package.swift
@@ -10,15 +10,15 @@ let package = Package(
         .library(name: "DcentBiometric", targets:["DcentBiometric"]),
     ],
     dependencies: [
-        .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
-        .package(name: "XCGLogger", url: "https://github.com/DaveWoodCom/XCGLogger.git", from: "7.0.1"),
-        .package(name: "BigInt", url: "https://github.com/attaswift/BigInt.git", from: "5.3.0")
+        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
+        .package(url: "https://github.com/DaveWoodCom/XCGLogger.git", from: "7.0.1"),
+        .package(url: "https://github.com/attaswift/BigInt.git", from: "5.3.0")
     ],
     targets: [
         //        Target
         .binaryTarget(name: "DcentBiometric", path: "DcentBiometric.xcframework"),
         .target(name: "DcentBiometricFrameworkBundle",
-                dependencies: [ .target(name: "DcentBiometric"), "SwiftProtobuf", "XCGLogger", "BigInt"],
+                dependencies: [ .target(name: "DcentBiometric"), .product(name: "SwiftProtobuf", package: "swift-protobuf"), .product(name: "XCGLogger", package: "XCGLogger"), .product(name: "BigInt", package: "BigInt")],
                 path: "DcentBiometricFrameworkBundle")
          
     ]
